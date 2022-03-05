425ebb8a2d85533afe01dc1f42ed49876a23e432
Shiran Terrazas <90222398+shiran-trrzs@users.noreply.github.com>
2022-03-04 20:30:30 -0500

    Revert "update: elimination of components from login"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ModulesModule } from './modules/modules.module';
-
 
 @NgModule({
   declarations: [
@@ -16,16 +11,12 @@ import { ModulesModule } from './modules/modules.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
-    MatFormFieldModule,
-    ModulesModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    }),
-    BrowserAnimationsModule
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
