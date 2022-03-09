8d6d433704e2dd4b1b9e2cf0ce9488658012fe42
shiran-trrzs <shirannnn.4@gmail.com>
2022-03-09 18:34:42 -0500

    Add: FormsModule for form implementation

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FormComponent } from './form/form.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { FormsModule } from '@angular/forms';
 import { QuoteComponent } from './quote/quote.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 
@@ -15,7 +16,8 @@ import { AboutUsComponent } from './about-us/about-us.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule
   ],
   exports: [
     FormComponent
