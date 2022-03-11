c03d65a6d4187683bba1f81cdef450838e190f1a
shiran-trrzs <shirannnn.4@gmail.com>
2022-03-10 20:42:08 -0500

    Add: Module chef and component chef-panel

---
+++ src/app/modules/chef/chef-panel/chef-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chef-panel',
+  templateUrl: './chef-panel.component.html',
+  styleUrls: ['./chef-panel.component.scss']
+})
+export class ChefPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/chef/chef.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChefPanelComponent } from './chef-panel/chef-panel.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChefPanelComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChefModule { }
