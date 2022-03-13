dd59bbf328b9502b927eb3d8d894d0a85b14433f
SharonNavarro <shanmowrk@gmail.com>
2022-03-13 17:40:50 -0500

    add: menu-view and orders-view components

--- src/app/modules/waiter/waiter.module.ts
+++ src/app/modules/waiter/waiter.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WaiterPanelComponent } from './waiter-panel/waiter-panel.component';
+import { MenuViewComponent } from './menu-view/menu-view.component';
+import { OrdersViewComponent } from './orders-view/orders-view.component';
 
 @NgModule({ 
   declarations: [
-    WaiterPanelComponent
+    WaiterPanelComponent,
+    MenuViewComponent,
+    OrdersViewComponent
   ],
   imports: [
     CommonModule
