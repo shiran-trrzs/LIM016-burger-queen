53c475033488f81ba989925a6a6335597e60536c
SharonNavarro <shanmowrk@gmail.com>
2022-03-14 18:32:26 -0500

    add: routing to menu

--- src/app/modules/waiter/menu-view/menu-view-routing.module.ts
+++ src/app/modules/waiter/menu-view/menu-view-routing.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllComponent } from './all/all.component';
+import { BreakfastComponent } from './breakfast/breakfast.component';
+import { LunchComponent } from './lunch/lunch.component';
 import { MenuViewComponent } from './menu-view.component';
-
-
 
 const routes: Routes = [
   { 
     path: '',
     component: MenuViewComponent,
+    children: [
+      {
+        path:'',
+        pathMatch:'full', 
+        redirectTo:'all'
+      },
+      {
+        path:'all',
+        component: AllComponent,
+      },
+      {
+        path:'lunch',
+        component: LunchComponent,
+      },
+      {
+        path:'breakfast',
+        component: BreakfastComponent,
+      }
+  ]
   }
 ];
 
