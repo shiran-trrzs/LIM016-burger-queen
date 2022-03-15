533ebadbc775fbf08b2f9f9610fd86a9d0f9c1c8
shiran-trrzs <shirannnn.4@gmail.com>
2022-03-14 19:29:51 -0500

    Add: Get and show all products for all component

--- src/app/modules/waiter/menu-view/all/all.component.ts
+++ src/app/modules/waiter/menu-view/all/all.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-all',
   templateUrl: './all.component.html',
   styleUrls: ['./all.component.scss']
 })
+
 export class AllComponent implements OnInit {
 
-  constructor() { }
+  allProducts: any = [];
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authService.getProducts().subscribe({
+      next: response => {
+        this.allProducts = response;
+        console.log(response)
+      },
+      error: error => {
+        console.error('There was an error!', error);
+    }
+    })
   }
 
 }
