5806bd8a9479eceb76129e4b0a34be8c28b7b05f
SharonNavarro <shanmowrk@gmail.com>
2022-03-15 09:47:58 -0500

    style: all component and fix route to logout

--- src/app/waiter-panel/waiter-panel.component.ts
+++ src/app/waiter-panel/waiter-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-waiter-panel',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaiterPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {} 
+
+  logoutUser() {
+    this.authService.logout();
   }
 
 }
