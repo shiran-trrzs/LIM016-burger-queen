28643a2bda61eb04a50baeb814df3531807e18b9
SharonNavarro <shanmowrk@gmail.com>
2022-03-21 08:58:43 -0500

    feat: show data from orders and design it

--- src/app/modules/chef/chef-view/chef-view.component.ts
+++ src/app/modules/chef/chef-view/chef-view.component.ts
@@ -8,11 +8,36 @@ import { AuthService,  } from 'src/app/_services/auth.service';
 })
 export class ChefViewComponent implements OnInit {
 
+  allOrders: any = [];
+
+  arrProducts: any = [];
+
+  kkk: any = [];
+
   constructor(
     private authService: AuthService
   ) {}
 
   ngOnInit(): void {
+
+    this.authService.getOrders().subscribe({
+      next: response => {
+        this.allOrders = response;
+        this.jjj()
+        console.log(this.allOrders)
+      },
+      error: error => {
+        console.error('There was an error!', error);
+    }
+    })
+
   }
+
+   jjj() {
+      this.allOrders.forEach((element: any) => {
+        this.arrProducts.push(element.products);
+         return this.arrProducts
+      });
+    }
 
 }
