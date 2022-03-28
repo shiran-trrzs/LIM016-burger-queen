e8c70f9d8315cf2d922eebd7f0408fbb60e342a6
shiran-trrzs <shirannnn.4@gmail.com>
2022-03-28 17:33:17 -0500

    Update: Change name from web

--- src/app/modules/waiter/orders-view/orders-view.component.spec.ts
+++ src/app/modules/waiter/orders-view/orders-view.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrdersViewComponent } from './orders-view.component';
-import { RouterTestingModule } from '@angular/router/testing'
+import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('OrdersViewComponent', () => {
