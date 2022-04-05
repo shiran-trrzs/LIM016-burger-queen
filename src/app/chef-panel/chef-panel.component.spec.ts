b015a87ebf0f619e6bcf134822a8bf262e25ca75
shiran-trrzs <shirannnn.4@gmail.com>
2022-04-05 00:39:17 -0500

    Test: Import Router

--- src/app/chef-panel/chef-panel.component.spec.ts
+++ src/app/chef-panel/chef-panel.component.spec.ts
@@ -5,13 +5,19 @@ import { RouterTestingModule } from '@angular/router/testing'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 describe('ChefPanelComponent', () => {
   let component: ChefPanelComponent;
   let fixture: ComponentFixture<ChefPanelComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async () => {    
+  // let routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate'])
+  await TestBed.configureTestingModule({
       declarations: [ ChefPanelComponent ],
+      // providers: [
+      //   { provide: Router }
+      // ],
       imports: [
         RouterTestingModule,
         HttpClientTestingModule
@@ -30,4 +36,10 @@ describe('ChefPanelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+//   it(`should navigate to nocustomer`, () => {
+//     let x = component.logoutUser()
+//     let routerSpy = jasmine.createSpyObj<Router>(['navigate'])
+//    expect (routerSpy.navigate).toBe(['/']);
+// });
 });
